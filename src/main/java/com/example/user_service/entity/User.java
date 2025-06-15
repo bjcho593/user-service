278a6a3e988fe51e61f16d1ab0a4ad9998bde011
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-15 15:08:28 -0500

    Agrega entidad User.java

---
+++ src/main/java/com/example/user_service/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String fullName;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String role = "USER"; // Puede ser "USER" o "ADMIN"
+}
