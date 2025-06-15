b10f9c43db8d82d60b936083fbd826a89a88c003
bjcho593 <155932533+bjcho593@users.noreply.github.com>
2025-06-15 15:44:18 -0500

    Agrega controlador UserController.java

---
+++ src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.entity.User;
+import com.example.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        return "User Service is running ✅";
+    }
+}
